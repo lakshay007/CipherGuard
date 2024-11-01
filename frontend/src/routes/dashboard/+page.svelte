4344208db2886465418d31907cea4724eebf61c4
Lakshay <dmslakshay@gmail.com>
2024-11-01 19:20:47 +0530

    urlscanner api link fix

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -24,7 +24,7 @@
 
         isLoading = true;
         try {
-            const response = await fetch('http://localhost:8000/predict', {
+            const response = await fetch('https://cguardurlapi.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
